dc2c51afe955acbb9f7d2553d876aac45b9aad10
hugococus <hugococus@hotmail.com>
2020-04-21 11:19:20 -0300

    Create pessoa.java

---
+++ pessoa.java
@@ -0,0 +1,12 @@
+package aula21042020;
+
+
+public class pessoa {
+    int id;
+    String nome;
+    String cpf;
+    
+    public void relatorio() {
+        System.out.println("id:" + this.id + " Nome: " + this.nome + " Cpf: " + this.cpf );
+    }
+}
